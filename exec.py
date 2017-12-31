10faa6a4f868cf9ff37a4de4997401b71f1c6f31
moskomule <hataya@keio.jp>
2017-12-31 15:13:07 +0900

    refactor add: l1,mse loss

--- exec.py
+++ exec.py
@@ -1,20 +1,21 @@
+import torch
 import torch.optim as optim
+from torch.nn import functional as F
 from torchvision import datasets, transforms
 
 from cifar import resnet20, preact_resnet20
-from utils import *
+from utils import naive_cross_entropy_loss, mixup_train, standard_train, test
 
 cuda_available = torch.cuda.is_available()
 
+losses = {"naive": naive_cross_entropy_loss,
+          "l1": F.l1_loss,
+          "mse": F.mse_loss}
 
-def main(use_mixup, alpha, batch_size, share, preact):
-    transform_for_mixup = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(32, padding=4),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-    transform_train = transforms.Compose([
+
+def get_loader(is_mixup, batch_size):
+    save_dir = "~/.torch/data/cifar10"
+    transform = transforms.Compose([
         transforms.RandomHorizontalFlip(),
         transforms.RandomCrop(32, padding=4),
         transforms.ToTensor(),
@@ -24,36 +25,50 @@ def main(use_mixup, alpha, batch_size, share, preact):
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
-    train_loader_1 = torch.utils.data.DataLoader(
-            datasets.CIFAR10('data/cifar10', train=True, download=True, transform=transform_for_mixup),
-            batch_size=batch_size,
-            shuffle=True)
-    train_loader_2 = torch.utils.data.DataLoader(
-            datasets.CIFAR10('data/cifar10', train=True, transform=transform_for_mixup), batch_size=batch_size,
-            shuffle=True)
-    train_loader = torch.utils.data.DataLoader(
-            datasets.CIFAR10('data/cifar10', train=True, download=True, transform=transform_train),
-            batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(
-            datasets.CIFAR10('data/cifar10', train=False, transform=transform_test), batch_size=batch_size,
+            datasets.CIFAR10(save_dir, train=False, transform=transform_test), batch_size=batch_size,
             shuffle=True)
+
+    if is_mixup:
+
+        train_loader_1 = torch.utils.data.DataLoader(
+                datasets.CIFAR10(save_dir, train=True, download=True, transform=transform),
+                batch_size=batch_size,
+                shuffle=True)
+        train_loader_2 = torch.utils.data.DataLoader(
+                datasets.CIFAR10(save_dir, train=True, transform=transform), batch_size=batch_size,
+                shuffle=True)
+        train_loader = (train_loader_1, train_loader_2)
+
+    else:
+
+        train_loader = torch.utils.data.DataLoader(
+                datasets.CIFAR10(save_dir, train=True, download=True, transform=transform),
+                batch_size=batch_size, shuffle=True)
+
+    return train_loader, test_loader
+
+
+def main(nomixup, alpha, batch_size, share, preact, lossf, lr):
+    train_loader, test_loader = get_loader(not nomixup, batch_size)
 
     model = preact_resnet20() if preact else resnet20()
     if cuda_available:
         model.cuda()
-    optimizer = optim.SGD(params=model.parameters(), lr=1e-1, momentum=0.9, weight_decay=1e-4)
+
+    optimizer = optim.SGD(params=model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150], gamma=0.1)
 
     for idx in range(200):
         print(f"epoch: {idx}")
         scheduler.step()
-        if use_mixup:
+        if not nomixup:
             mixup_train(model=model, optimizer=optimizer,
-                        data_loaders=(train_loader_1, train_loader_2), alpha=alpha,
-                        data_length=len(train_loader_1), share_lambda=share)
+                        data_loaders=train_loader, alpha=alpha,
+                        data_length=len(train_loader[0]), share_lambda=share, loss_f=losses[lossf])
         else:
             standard_train(model=model, optimizer=optimizer,
-                           data_loader=train_loader, data_length=len(train_loader))
+                           data_loader=train_loader, data_length=len(train_loader), loss_f=losses[lossf])
         test(model=model, data_loader=test_loader)
 
 
@@ -61,10 +76,12 @@ if __name__ == '__main__':
     import argparse
 
     p = argparse.ArgumentParser()
-    p.add_argument("--nomixup", action="store_false")
+    p.add_argument("--nomixup", action="store_true")
     p.add_argument("--alpha", type=float, default=1)
     p.add_argument("--batchsize", type=int, default=64)
     p.add_argument("--share", action="store_true")
     p.add_argument("--preact", action="store_true")
+    p.add_argument("--lossf", default="naive")
+    p.add_argument("--lr", type=float, default=1e-1)
     args = p.parse_args()
-    main(args.nomixup, args.alpha, args.batchsize, args.share, args.preact)
+    main(**vars(args))
--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ from functions import variable, onehot, naive_cross_entropy_loss
 cuda_available = torch.cuda.is_available()
 
 
-def standard_train(model, optimizer, data_loader, data_length):
+def standard_train(model, optimizer, data_loader, data_length, loss_f):
     """
     train in standard way
     """
@@ -19,14 +19,15 @@ def standard_train(model, optimizer, data_loader, data_length):
             input = variable(input)
             target = variable(onehot(target, 10))
         optimizer.zero_grad()
-        loss = naive_cross_entropy_loss(model(input), target)
+        loss = loss_f(model(input), target)
         loss.backward()
         optimizer.step()
         loop_loss.append(loss.data[0] / data_length)
     print(f">>>(standard)loss: {sum(loop_loss):.2f}")
 
 
-def mixup_train(model, optimizer, data_loaders, alpha, data_length, num_classes=10, share_lambda=False):
+def mixup_train(model, optimizer, data_loaders, alpha, data_length, num_classes=10, share_lambda=False,
+                loss_f=naive_cross_entropy_loss):
     """
     train function for mixup
     """
@@ -39,19 +40,16 @@ def mixup_train(model, optimizer, data_loaders, alpha, data_length, num_classes=
         if share_lambda:
             # share a same lambda in each minibatch
             _lambda = random.beta(alpha, alpha)
-            _input_lambda, _target_lambda = _lambda, _lambda
         else:
             _lambda = torch.Tensor(random.beta(alpha, alpha, size=input1.size()[0]))
-            _input_lambda = _lambda.view(-1, 1, 1, 1)
-            _target_lambda = _lambda.view(-1, 1)
 
-        input = _input_lambda * input1 + (1 - _input_lambda) * input2
-        target = _target_lambda * target1 + (1 - _target_lambda) * target2
+        input = _lambda * input1 + (1 - _lambda) * input2
+        target = _lambda * target1 + (1 - _lambda) * target2
 
         input = variable(input)
         target = variable(target)
         optimizer.zero_grad()
-        loss = naive_cross_entropy_loss(model(input), target)
+        loss = loss_f(model(input), target)
         loss.backward()
         optimizer.step()
         loop_loss.append(loss.data[0] / data_length)
