1e0e882610340d8a776968cae34e86334d2dffad
moskomule <hataya@keio.jp>
2020-08-06 23:03:08 +0900

    update

--- README.md
+++ README.md
@@ -3,19 +3,23 @@
 An implementation of *mixup: Beyond Empirical Risk Minimization* by Hongyi Zhang, Moustapha Cisse, Yann N. Dauphin, and David Lopez-Paz.
 
 ```
-python main.py [--batch_size 128 --epochs 400 --steps 250 300 --dataset cifar10 --model resnet20 --alpha 1 [--baseline]]
+python main.py model.mixup={true,false} model.input_only={true,false}
 ```
+
+mixup can (mathematically) avoid mixing labels by replacing `beta(alpha, alpha)` with `beta(alpha+1, alpha)`. `model.input_only=true` is to confirm this. (*)
 
 ## Requirements
 
-* PyTorch v1.0
-* torchvision (v0.2.2)
-* homura `pip install -U git+https://github.com/moskoumule/homura` (*Use dev!*)
+* PyTorch==1.6.0
+* torchvision==0.7.0
+* homura `pip install -U git+https://github.com/moskoumule/homura@v2020.08`
 
 ## Results
 
 ### CIFAR10 on ResNet-20
 
-|                  | EMP            | mixup          |
-|:-------------    | :------------- | :------------- |
-|max. test accuracy|  92%           | 93%            |
+|   ERM   |  mixup    |  mixup (`input_only=true`) |
+|:--- |:--- |:--- |
+|  0.923  | 0.932    |  0.931                      |
+
+The results suggest that the alternative mixup strategy (*) is as effective as the original. 
--- main.py
+++ main.py
@@ -1,81 +1,79 @@
+from typing import Tuple
+
+import hydra
+import numpy as np
 import torch
-from homura import trainers, reporters, optim, callbacks, lr_scheduler, Map
-from homura.modules import to_onehot
-from homura.vision.data import cifar10_loaders, cifar100_loaders
-from homura.vision.models import resnet20, wrn28_2
-from numpy.random import beta
+from homura import trainers, optim, lr_scheduler
+from homura.metrics import accuracy
+from homura.vision import DATASET_REGISTRY, MODEL_REGISTRY
 from torch.nn import functional as F
 
-MODELS = {"resnet20": resnet20,
-          "wrn28_2": wrn28_2}
 
-DATASETS = {"cifar10": cifar10_loaders,
-            "cifar100": cifar100_loaders}
-
-NUMCLASSES = {"cifar10": 10,
-              "cifar100": 100}
-
-
-def mixup(input, target, gamma):
-    # target is onehot format!
-    perm = torch.randperm(input.size(0))
-    perm_input = input[perm]
-    perm_target = target[perm]
-    return input.mul_(gamma).add_(1 - gamma, perm_input), target.mul_(gamma).add_(1 - gamma, perm_target)
+def partial_mixup(input: torch.Tensor,
+                  gamma: float,
+                  indices: torch.Tensor
+                  ) -> torch.Tensor:
+    if input.size(0) != indices.size(0):
+        raise RuntimeError("Size mismatch!")
+    perm_input = input[indices]
+    return input.mul(gamma).add(perm_input, alpha=1 - gamma)
 
 
-def naive_cross_entropy_loss(input, target):
-    return - (input.log_softmax(dim=-1) * target).sum(dim=-1).mean()
+def mixup(input: torch.Tensor,
+          target: torch.Tensor,
+          gamma: float,
+          ) -> Tuple[torch.Tensor, torch.Tensor]:
+    indices = torch.randperm(input.size(0), device=input.device, dtype=torch.long)
+    return partial_mixup(input, gamma, indices), partial_mixup(target, gamma, indices)
 
 
-class MixupTrainer(trainers.SupervisedTrainer):
-    def iteration(self, data):
+def naive_cross_entropy_loss(input: torch.Tensor,
+                             target: torch.Tensor
+                             ) -> torch.Tensor:
+    return -(input.log_softmax(dim=-1) * target).sum(dim=-1).mean()
+
+
+class Trainer(trainers.SupervisedTrainer):
+    def iteration(self,
+                  data):
         input, target = data
+        _target = target
+        target = F.one_hot(target, self.num_classes)
+        if self.is_train and self.cfg.mixup:
+            if self.cfg.input_only:
+                input = partial_mixup(input, np.random.beta(self.cfg.alpha + 1, self.cfg.alpha),
+                                      torch.randperm(input.size(0), device=input.device, dtype=torch.long))
+            else:
+                input, target = mixup(input, target, np.random.beta(self.cfg.alpha, self.cfg.alpha))
+
+        output = self.model(input)
+        loss = self.loss_f(output, target)
+
+        self.reporter.add('loss', loss.detach())
+        self.reporter.add('accuracy', accuracy(output, _target))
+
         if self.is_train:
-            input, target = mixup(input, to_onehot(target, self.num_classes),
-                                  beta(self.alpha, self.alpha))
-            output = self.model(input)
-            loss = self.loss_f(output, target)
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
-        else:
-            output = self.model(input)
-            loss = F.cross_entropy(output, target)
-        results = Map(loss=loss, output=output)
-        return results
 
 
-def main():
-    Trainer = trainers.SupervisedTrainer if args.baseline else MixupTrainer
-    model = MODELS[args.model](num_classes=NUMCLASSES[args.dataset])
-    train_loader, test_loader = DATASETS[args.dataset](args.batch_size)
-    optimizer = optim.SGD(lr=1e-1, momentum=0.9, weight_decay=1e-4)
-    scheduler = lr_scheduler.MultiStepLR(args.steps, gamma=0.1)
-    c = [callbacks.AccuracyCallback(), callbacks.LossCallback()]
+@hydra.main("main.yaml")
+def main(cfg):
+    train_loader, test_loader, num_classes = DATASET_REGISTRY(cfg.data.name)(cfg.data.batch_size,
+                                                                             return_num_classes=True,
+                                                                             num_workers=4)
+    model = MODEL_REGISTRY(cfg.model.name)(num_classes=num_classes)
+    optimizer = optim.SGD(lr=1e-1, momentum=0.9, weight_decay=cfg.optim.weight_decay)
+    scheduler = lr_scheduler.CosineAnnealingWithWarmup(200, 4, 5)
 
-    with reporters.TQDMReporter(range(args.epochs), callbacks=c) as tq, reporters.TensorboardReporter(c) as tb:
-        trainer = Trainer(model, optimizer, F.cross_entropy if args.baseline else naive_cross_entropy_loss,
-                          callbacks=[tq, tb],
-                          scheduler=scheduler,
-                          alpha=args.alpha,
-                          num_classes=NUMCLASSES[args.dataset])
-        for _ in tq:
+    with Trainer(model, optimizer, naive_cross_entropy_loss, scheduler=scheduler, cfg=cfg.model,
+                 num_classes=num_classes) as trainer:
+        for _ in trainer.epoch_range(200):
             trainer.train(train_loader)
             trainer.test(test_loader)
+        print(f"Max Test Accuracy={max(trainer.reporter.history('accuracy/test')):.4f}")
 
 
 if __name__ == '__main__':
-    import miniargs
-
-    p = miniargs.ArgumentParser()
-    p.add_int("--batch_size", default=128)
-    p.add_int("--epochs", default=200)
-    p.add_multi_int("--steps", default=[100, 150])
-    p.add_str("--dataset", choices=list(DATASETS.keys()))
-    p.add_str("--model", choices=list(MODELS.keys()))
-    p.add_float("--alpha", default=1)
-    p.add_true("--baseline")
-    args = p.parse()
-
     main()
